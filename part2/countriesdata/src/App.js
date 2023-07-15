f145628c6665e09c239683303021a1d4ad1be9c5
An Huynh <thaianhuynh12345@gmail.com>
2023-07-15 02:15:21 -0600

    Implemented search box and query results for countriesdata project

--- part2/countriesdata/src/App.js
+++ part2/countriesdata/src/App.js
@@ -1,14 +1,65 @@
 import {useState, useEffect} from 'react'
 import countriesServices from './services/countries'
 
+
+const Countries = ({countries, findCountry, country}) => {
+  if(!countries || findCountry === ''){
+    return null
+  }
+
+  if(countries.length > 10){
+    return(
+      <div>
+        Too many results, please be more specific
+      </div>
+    )
+  }else if(countries.length === 1 && country !== null){
+    console.log(country.flags.png)
+    return(
+      <div>
+        <h1>{country.name.common}</h1>
+        <span></span>
+        <span></span>
+        <div>Capital: {country.capital}</div>
+         <div>Area: {country.area}</div>
+        <h3>Languages</h3>
+        <ul>
+          {Object.values(country.languages).map(language => (
+              <li>{language}</li>
+          ))}
+        </ul>
+        <img src={`${(country.flags.png)}`} />
+      </div>
+    )
+  }
+  
+}
+
+
+
+
 function App() {
   const [findCountry, setFindCountry] = useState('')
   const [countries, setCountries] = useState(null)
-  const [countriesList, setCountriesList] = useState([])
+  const [country, setCountry] = useState(null)
+  const [countriesList, setCountriesList] = useState(null)
+
 
   const handleInputChange = (event) => {
     setFindCountry(event.target.value)
+    setCountry(null)
+    setCountriesList(countries.filter(country => country.common.toLowerCase().includes(findCountry.toLowerCase())))
+    if(countriesList){
+      if(countriesList.length === 1){
+        countriesServices.getCountry(countriesList[0].common.toLowerCase())
+                          .then(response => {
+                            setCountry(response.data)
+                          })
+                          .catch(error => null)
+      }
+    }
   }
+
 
   useEffect(() => {
     countriesServices.getCountries()
@@ -21,18 +72,20 @@ function App() {
     return null
   }
 
-  console.log(countries)
 
-  if(findCountry != ''){
-    setCountriesList(countries.map(country => {
-      
-    }))
-  }
+  
+
+  // setCountriesList(countries.filter(country => {
+  //   if(country.common.toLowerCase() === findCountry.toLowerCase()){
+  //     return country
+  //   }
+  // }))
 
   return (
     <div>
       <span>Find the countries: </span>
       <input value={findCountry} onChange={handleInputChange} />
+      <Countries countries={countriesList} findCountry={findCountry} country={country}/>
     </div>
   );
 }
--- part2/countriesdata/src/services/countries.js
+++ part2/countriesdata/src/services/countries.js
@@ -1,11 +1,15 @@
 import axios from 'axios'
 
-const url = 'https://studies.cs.helsinki.fi/restcountries/api/all'
+const url = 'https://studies.cs.helsinki.fi/restcountries/api/'
 
 const getCountries = () => {
-    return(axios.get(url)
+    return(axios.get(`${url}all`)
                 .then(response => response.data))
 }
 
+const getCountry = (country)=> {
+    return(axios.get(`${url}name/${country}`))
+}
 
-export default {getCountries}
+
+export default {getCountries, getCountry}
