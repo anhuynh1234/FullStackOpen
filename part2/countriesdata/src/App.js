6ad7209fe4690e9cd7638ee04195551b2ff53b48
An Huynh <thaianhuynh1234@outlook.com>
2023-07-15 17:45:55 -0600

    Implemented extra features in countries data: - Added Show button next to country items in a list to show single country data - Added weather data features - Added axios request command to weather API to get weather data

--- part2/countriesdata/src/App.js
+++ part2/countriesdata/src/App.js
@@ -2,9 +2,22 @@ import {useState, useEffect} from 'react'
 import countriesServices from './services/countries'
 
 
-const Countries = ({countries, findCountry, country}) => {
+const Countries = ({countries, findCountry, country, handleClickShow, weather}) => {
   if(!countries || findCountry === ''){
     return null
+  }
+
+  let weatherData = (<></>)
+
+  if(weather){
+    weatherData = (
+      <div>
+        <p>Temperature: {weather.current.temp_c} C ({weather.current.temp_f} F)</p>
+        <p>Condition: {weather.current.condition.text}</p>
+        <img src={weather.current.condition.icon}/>
+        <p>Wind speed: {weather.current.wind_kph} kph ({weather.current.wind_mph} mph)</p>
+      </div>
+    )
   }
 
   if(countries.length > 10){
@@ -13,8 +26,7 @@ const Countries = ({countries, findCountry, country}) => {
         Too many results, please be more specific
       </div>
     )
-  }else if(countries.length === 1 && country !== null){
-    console.log(country.flags.png)
+  }else if(countries.length === 1 && country){
     return(
       <div>
         <h1>{country.name.common}</h1>
@@ -25,17 +37,61 @@ const Countries = ({countries, findCountry, country}) => {
         <h3>Languages</h3>
         <ul>
           {Object.values(country.languages).map(language => (
-              <li>{language}</li>
+              <li key={language}>{language}</li>
           ))}
         </ul>
-        <img src={`${(country.flags.png)}`} />
+        <img src={`${(country.flags.png)}`} alt={`${(country.flags.alt)}`} />
+        {weatherData}
       </div>
     )
+  }else{
+    if(country !== null){
+      return(
+      <>
+        <div>
+          {countries.map(singleCountry => {
+            return(
+              <div key={singleCountry.common}>
+              {singleCountry.common}
+              <span>  </span>
+              <button value={singleCountry.common} onClick={handleClickShow}>Show me</button>
+              </div>)})}
+          </div>
+          <div>  
+            <h1>{country.name.common}</h1>
+            <span></span>
+            <span></span>
+            <div>Capital: {country.capital}</div>
+            <div>Area: {country.area}</div>
+            <h3>Languages</h3>
+            <ul>
+              {Object.values(country.languages).map(language => (
+                  <li key={language}>{language}</li>
+              ))}
+            </ul>
+            <img src={`${(country.flags.png)}`} alt={`${(country.flags.alt)}`} />
+            {weatherData}
+          </div>
+        </>
+      )
+    }else {
+      return(
+        <div>
+          {countries.map(singleCountry => {
+            return(
+              <div key={singleCountry.common}>
+                {singleCountry.common}
+                <span>  </span>
+                <button value={singleCountry.common} onClick={handleClickShow}>Show me</button>
+              </div>
+            )
+          })}
+        </div>
+      )
+    }
   }
   
 }
-
-
 
 
 function App() {
@@ -43,21 +99,12 @@ function App() {
   const [countries, setCountries] = useState(null)
   const [country, setCountry] = useState(null)
   const [countriesList, setCountriesList] = useState(null)
+  const [weather, setWeather] = useState(null)
+  const [singleCountry, setSingleCountry] = useState(null)
 
 
   const handleInputChange = (event) => {
     setFindCountry(event.target.value)
-    setCountry(null)
-    setCountriesList(countries.filter(country => country.common.toLowerCase().includes(findCountry.toLowerCase())))
-    if(countriesList){
-      if(countriesList.length === 1){
-        countriesServices.getCountry(countriesList[0].common.toLowerCase())
-                          .then(response => {
-                            setCountry(response.data)
-                          })
-                          .catch(error => null)
-      }
-    }
   }
 
 
@@ -68,12 +115,58 @@ function App() {
                           })
   },[])
 
+  useEffect(() => {
+    // setCountry(null)
+    if(countries !== null){
+      setCountriesList(countries.filter(country => country.common.toLowerCase().includes(findCountry.toLowerCase())))
+      setSingleCountry(null)
+    }
+  }, [findCountry, countries])
+  
+  useEffect(() => {
+    if(countriesList !== null){
+      if(countriesList.length === 1){
+        countriesServices.getCountry(countriesList[0].common.toLowerCase())
+        .then(response => {
+          setCountry(response.data)
+        })
+        .catch(error => null)
+      }else{
+        setCountry(null)
+      }
+    }
+  }, [countriesList])
+
+  useEffect(() => {
+    if(singleCountry){
+      countriesServices.getCountry(singleCountry.toLowerCase())
+          .then(response => {
+            setCountry(response.data)
+          })
+          .catch(error => null)
+    }
+  }, [singleCountry])
+
+  useEffect(() => {
+    if(country){
+      countriesServices.getWeather(country.name.common)
+                        .then(data => {
+                          setWeather(data)
+                        })
+                        .catch(error => null)
+    }else{
+      setWeather(null)
+    }
+  }, [country])
+
   if(!countries){
     return null
   }
 
-
-  
+  const handleClickShow = (event) => {
+    event.preventDefault()
+    setSingleCountry(event.target.value)
+  }
 
   // setCountriesList(countries.filter(country => {
   //   if(country.common.toLowerCase() === findCountry.toLowerCase()){
@@ -85,7 +178,7 @@ function App() {
     <div>
       <span>Find the countries: </span>
       <input value={findCountry} onChange={handleInputChange} />
-      <Countries countries={countriesList} findCountry={findCountry} country={country}/>
+      <Countries countries={countriesList} findCountry={findCountry} country={country} handleClickShow={handleClickShow} weather={weather} />
     </div>
   );
 }
--- part2/countriesdata/src/services/countries.js
+++ part2/countriesdata/src/services/countries.js
@@ -2,6 +2,22 @@ import axios from 'axios'
 
 const url = 'https://studies.cs.helsinki.fi/restcountries/api/'
 
+
+const getWeather  =(location) => {
+    const options = {
+        method: 'GET',
+        url: 'https://weatherapi-com.p.rapidapi.com/current.json',
+        params: {q: location},
+        headers: {
+          'X-RapidAPI-Key': 'ab48191ae1msh7eaff8b2cd8c03ap1ac406jsn2fdf06ac0b14',
+          'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+        }
+      };
+      
+      return(axios.request(options)
+                    .then(response => response.data))
+}
+
 const getCountries = () => {
     return(axios.get(`${url}all`)
                 .then(response => response.data))
@@ -12,4 +28,4 @@ const getCountry = (country)=> {
 }
 
 
-export default {getCountries, getCountry}
+export default {getCountries, getCountry, getWeather}
