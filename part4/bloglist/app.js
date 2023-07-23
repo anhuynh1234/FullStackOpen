328e8427db137d8e857645eb96570915f9eb0adc
An Huynh <thaianhuynh1234@outlook.com>
2023-07-23 14:42:23 -0600

    Initialied and refactored blog list application -Separated components into different modules -Connected database to server with MongoDB

---
+++ part4/bloglist/app.js
@@ -0,0 +1,24 @@
+const {PORT, MONGO_URI} = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
+const blogRouter = require('./controllers/blog')
+
+const mongoUrl = MONGO_URI
+mongoose.connect(mongoUrl)
+        .then(result => {
+            console.log('Connected to MongoDB')
+        })
+        .catch(error => {
+            console.log('Error found: ' + error.message)
+        })
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+
+app.use('/api/blogs', blogRouter)
+
+module.exports = app
