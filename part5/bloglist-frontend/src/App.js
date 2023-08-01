77963776f8fc827248007c18a2e1829660658fb4
An Huynh <thaianhuynh1234@outlook.com>
2023-08-01 13:05:07 -0600

    Initialized bloglist frontend with login form

---
+++ part5/bloglist-frontend/src/App.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  const handleLogin = (event) => {
+    event.preventDefault()
+    console.log('Logging in with', username, password)
+  }
+
+  return (
+    <div>
+      <h2>Blogs</h2>
+
+      <form onSubmit={handleLogin}>
+        <div>
+          Username  
+            <input type="text" value={username} name="Username" onChange={target => {setUsername(target.target.value)}} />
+        </div> 
+        <div>
+          Password    
+            <input type='password' value={password} name="Password" onChange={target => {setPassword(target.target.value)}} />
+        </div>
+        <button type='submit'>Log in</button>
+      </form>
+
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+}
+
+export default App
