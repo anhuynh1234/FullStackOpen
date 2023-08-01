d98c0f2c13b2d4f79843546e5c8ea1eed41b0749
An Huynh <thaianhuynh1234@outlook.com>
2023-08-01 13:31:31 -0600

    Implemented login services for bloglist frontend

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,11 +1,18 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
+
+const Error = ({errorMessage}) => {
+
+}
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -13,27 +20,44 @@ const App = () => {
     )  
   }, [])
 
-  const handleLogin = (event) => {
+  const handleLogin =async (event) => {
     event.preventDefault()
-    console.log('Logging in with', username, password)
+
+    try {
+      const user = await loginService.login({username, password,})
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeOut(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  if(user === null){
+    return (
+      <div>
+        <h2>Blogs</h2>
+
+        <form onSubmit={handleLogin}>
+          <div>
+            Username  
+              <input type="text" value={username} name="Username" onChange={target => {setUsername(target.target.value)}} />
+          </div> 
+          <div>
+            Password    
+              <input type='password' value={password} name="Password" onChange={target => {setPassword(target.target.value)}} />
+          </div>
+          <button type='submit'>Log in</button>
+        </form>
+      </div>
+    )
   }
 
   return (
-    <div>
-      <h2>Blogs</h2>
-
-      <form onSubmit={handleLogin}>
-        <div>
-          Username  
-            <input type="text" value={username} name="Username" onChange={target => {setUsername(target.target.value)}} />
-        </div> 
-        <div>
-          Password    
-            <input type='password' value={password} name="Password" onChange={target => {setPassword(target.target.value)}} />
-        </div>
-        <button type='submit'>Log in</button>
-      </form>
-
+   <div>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
